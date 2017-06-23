f254755d2b16bb4abfc77054e59a8c4fd9ee2d04
Grégoire Gaonach <gregoire.gaonach@isen.yncrea.fr>
2017-06-23 16:36:44 +0200

    Update premierTest.py

--- premierTest.py
+++ premierTest.py
@@ -13,11 +13,12 @@ LED_COUNT      = 256      # Nombre de LED
 LED_PIN        = 10       # Pin de connection : mettre le GPIO 10 pour SPI (/dev/spidev0.0)
 LED_FREQ_HZ    = 800000   # Frequence du signal en hertz (defaut : 800khz)
 LED_DMA        = 5        # Chaine DMA pour generer le signal (defaut : 5)
-LED_BRIGHTNESS = 20       # Luminosite de 0 a 255
+LED_BRIGHTNESS = 15       # Luminosite de 0 a 255
 LED_INVERT     = False    # True pour inverser le signal
 LED_CHANNEL    = 0        # Mettre 1 pour les GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Type de matrice et ordre des couleurs (defaut : ws.WS2811_STRIP_GRB)
-
+ 
+ 
 """ LISTES DES FONTIONS UTILES AU PROGRAMME 
     =======================================
 """
@@ -32,13 +33,15 @@ LED_STRIP      = ws.WS2811_STRIP_GRB   # Type de matrice et ordre des couleurs (
 	matrix : matrice ligne 1D contenant les informations de toutes les LED a afficher une a une
 """
 def displayMatrix(offset, matrix):
-		
+	
+	print matrix
+	
 	offset = 8*offset
-		
+
 	for i in range(0, len(matrix)):
 		if matrix[i] > 0 and matrix[i] <= 255:
 			matrix[i] = Color(200,200,200)
-								
+
 		strip.setPixelColor(i + offset, matrix[i])
 		
 	strip.show()
@@ -53,15 +56,14 @@ def displayMatrix(offset, matrix):
     matrix : matrice ligne 1D contenant les informations de toutes les LED a afficher une a une
 """
 def scroll(offset, matrix, speed = 1):	
-	matrixBuffer = matrix
 	
-	while len(matrixBuffer) > 8:
-		del matrixBuffer[:16]
+	while True:
+		displayMatrix(offset, matrix)
+					
+		matrix += matrix[:16]
+		del matrix[:16]
 	
-		displayMatrix(offset, matrixBuffer)
-	
-		print matrixBuffer
-		time.sleep(speed)
+		time.sleep(50/1000.0)
 			
 def twitterLogo():
 	
@@ -89,20 +91,20 @@ def matrixToLine(matrix):
 	
 	return matrix.tolist()
 	
-def displayText(offset, text, icone = True):
+def displayText(offset, text, font = "SMALL", icone = True):
 	# Definition du texte et de sa taille :
-	font = ImageFont.truetype("fonts/TINY_FONT.ttf", 5)
+	font = ImageFont.truetype("fonts/"+font+"_FONT.ttf", 8)
 	wText, hText = font.getsize(text)
 	# Creation d'une image de la taille du texte
 	image = Image.new("L", (wText, 8), "black")
 	draw = ImageDraw.Draw(image)
-	draw.text((0, 1), text, fill="white", font=font)
+	draw.text((0, 0), text, fill="white", font=font)
 	# Retourne une matrice bitmap si le text est petit / sinon fait defiler le text
 	maxLED = 22 if icone else 32
 	if wText <= maxLED:
 		return displayMatrix(offset, matrixToLine(np.array(image)))
 	else:
-		return scroll(offset, matrixToLine(np.array(image)))
+		return scroll(offset, matrixToLine(np.array(image)), speed = 3)
 
 """ PROGRAMME PRINCIPAL
     ===================
@@ -116,5 +118,5 @@ if __name__ == '__main__':
 	strip.begin()
 	
 	displayMatrix(0, twitterLogo())
-	displayText(10, "Cerise gout pelican")
+	displayText(10, "This  is a simple hello world with a long long text ! ", font="SMALL")
 
